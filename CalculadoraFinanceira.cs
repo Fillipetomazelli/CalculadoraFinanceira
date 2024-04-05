672234483546ec13a881ed8bbf702448b8445756
Fillipetomazelli <160272085+Fillipetomazelli@users.noreply.github.com>
2024-04-05 14:44:20 -0400

    Add files via upload

---
+++ CalculadoraFinanceira.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinhaBiblioteca2
+{
+    public static class CalculadoraFinanceira
+    {
+        public static double Montante(double capital, double taxa, double tempo)
+        {
+            taxa = taxa / 100.0;
+            double montante = capital * (1 + taxa * tempo); 
+            return montante;
+        }
+        public static double Capital(double montante, double taxa, double tempo)
+        {
+            taxa = taxa / 100.0;
+            double capital = montante / (1 + taxa * tempo);
+            return capital;
+        }
+        public static double Juros(double montante, double capital)
+        {
+            
+            double juros = montante - capital;
+            return juros;
+        }
+        public static double Taxa(double montante, double capital, double tempo)
+        {
+
+            double taxa = montante - capital / (capital * tempo);
+            return taxa/100.0;
+        }
+        public static double Tempo(double montante, double capital, double taxa)
+        {
+            taxa = taxa / 100.0;
+            double tempo = montante - capital / (capital * taxa);
+            return tempo;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿using MinhaBiblioteca2;
+
+
+while (true) 
+{
+    Console.WriteLine("\n\nCalculadora Financeira");
+    Console.WriteLine("\n1 - CALCULAR MONTANTE");
+    Console.WriteLine("2 - CALCULAR CAPITAL");
+    Console.WriteLine("3 - CALCULAR TAXA");
+    Console.WriteLine("4 - CALCULAR JUROS");
+    Console.WriteLine("5 - CALCULAR TEMPO");
+
+    Console.Write("\n\nEscolha uma opção: ");
+    byte opcao = Convert.ToByte(Console.ReadLine());
+
+    switch (opcao)
+    {
+        case 1:
+            Console.Write("\nDigite o Capital: ");
+            double capital = Convert.ToDouble(Console.ReadLine());
+            Console.Write("\nDigite o taxa: ");
+            double taxa = Convert.ToDouble(Console.ReadLine());
+            Console.Write("\nDigite o tempo: ");
+            double tempo = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("\n O valor do Montante será = " + CalculadoraFinanceira.Montante(capital, taxa, tempo)); 
+            break;
+        case 2:
+            Console.Write("\nDigite o Montante: ");
+            double montante = Convert.ToDouble(Console.ReadLine());
+            Console.Write("\nDigite o taxa: ");
+            taxa = Convert.ToDouble(Console.ReadLine());
+            Console.Write("\nDigite o tempo: ");
+            tempo = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("\n O valor do Capital será = " + CalculadoraFinanceira.Capital(montante, taxa, tempo)); 
+            break;
+        case 3:
+            Console.Write("\nDigite o Montante: ");
+            montante = Convert.ToDouble(Console.ReadLine());
+            Console.Write("\nDigite o capital: ");
+            capital = Convert.ToDouble(Console.ReadLine());
+            Console.Write("\nDigite o tempo: ");
+            tempo = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("\n O valor da taxa será = " + CalculadoraFinanceira.Taxa(montante, capital, tempo));
+            break;
+        case 4:
+            Console.Write("\nDigite o Montante: ");
+            montante = Convert.ToDouble(Console.ReadLine());
+            Console.Write("\nDigite o Capital: ");
+            capital = Convert.ToDouble(Console.ReadLine());
+         
+            Console.WriteLine("\n O valor do Juros será = " + CalculadoraFinanceira.Juros(montante, capital));
+            break;
+        case 5:
+            Console.Write("\nDigite o Montante: ");
+            montante = Convert.ToDouble(Console.ReadLine());
+            Console.Write("\nDigite o capital: ");
+            capital = Convert.ToDouble(Console.ReadLine());
+            Console.Write("\nDigite o taxa: ");
+            taxa = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("\nO tempo será = " + CalculadoraFinanceira.Tempo(montante,capital, taxa));
+            break;
+    }
+        
+   
+
+    
+}
